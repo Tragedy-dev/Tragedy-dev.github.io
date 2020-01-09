2090b0847a750d344e5f84a2f66152172d1f4f89
Tragedy <54042397+Tragedy-dev@users.noreply.github.com>
2020-01-08 19:30:03 -0600

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,79 +1,124 @@
 // JavaScript source code
 var level = 1
 var points = 0
-var progressBarProgress = 10
-var pointsPerClick = 10
+var progressBarProgress = 10 // (a percent)
+var pointsPerClick = 1
 var pointsPerLevel = 10
 var upgradeCost = 15
-
+var upgradeText = ["Buy an extra arm <br/> from the arm dealer", "take a class in genetic engineering and grow an arm on your ear", "Gain a free level", "Steal a robot from a secret lab"];
+var dot = 0
+var upgrades = 0
+var robot = "flase"
+ 
 function click() {
-points = points + 1;
+points = points + pointsPerClick;
 document.getElementById("butt").innerHTML = "You have " + points + " points";
 document.getElementById("myBar").style.width = progressBarProgress + "%";
-progressBarProgress = progressBarProgress + pointsPerClick;
+progressBarProgress = + progressBarProgress + (pointsPerClick / pointsPerLevel) * 100;
+
 }
 
+
 function myFunction() {
+/* just setting stuff so it goes back to norms */
 document.getElementById("levelText").style.fontSize = "10px";
 document.getElementById("levelText").style.marginTop= "-15px";
 document.getElementById("levelText").style.color = "black";
 document.getElementById("levelText").style.backgroundColor = "white";
 document.getElementById("levelText").innerHTML = "Level " + level;
 document.getElementById("levelText").style.webkitAnimation = "none";
+document.getElementById("buttt").style.display = "none";
+document.getElementById("butt").style.webkitAnimation = "none";
+// levl up reset below
 if (points >= pointsPerLevel) {
-/* alert("LEVEL UP!"); */
-points = -1;
-progressBarProgress = -10;
-progressBarProgress = progressBarProgress + pointsPerClick;
+points = 0;
+progressBarProgress = pointsPerClick / pointsPerLevel * 100;
+//progressBarProgress = progressBarProgress + pointsPerClick;
 level = level + 1;
+pointsPerLevel = pointsPerLevel * 1.2;
+//fancylevel up stuff
 document.getElementById("levelText").innerHTML = "!! Level " + level + " !!";
 document.getElementById("levelText").style.webkitAnimation = "colorchange 5s infinite alternate";
-document.getElementById("butt").innerHTML = "Level Up!";
+document.getElementById("butt").innerHTML = "Level Up!"; 
+document.getElementById("butt").style.webkitAnimation = "colorchange 5s infinite alternate";
+
+//upgrade text
+
+if (points >= 1)
+upgradeText = upgradeText[dot]
+document.getElementById("upgradeText").innerHTML = upgradeText[dot];
 }
-else if (points >= pointsPerLevel, progressBarProgress > 100) {
+
+//limit on progress bar
+/*else if (points >= pointsPerLevel && progressBarProgress >= 100) {
 points = points - 1;
 progressBarProgress = progressBarProgress - pointsPerClick;
-} else {
+} 
+*/
+/*get a point below and varius stuff in click() function*/
+ 
+else {
 click()
+}
+
+
+/*Upgrade section stuff below*/
+if (level >= 3) {
+document.getElementById("buttt").style.display = "block";
 }
 }
 
+//mouseover stuff and such
+
 function mouseOver() {
   document.getElementById("but").style.color = "white";
-    document.getElementById("but").style.border = "lightgreen dashed 5px";
+    document.getElementById("but").style.border = "lightgreen solid 2px";
 }
 
 function mouseOut() {
   document.getElementById("but").style.color = "black";
-    document.getElementById("but").style.border = "grey solid 5px";
+    document.getElementById("but").style.border = "grey solid 2px";
 }
 
 function mouseScroll() {
   document.getElementById("but").style.color = "blue";
-document.getElementById("but").style.border = "blue solid 5px";
+document.getElementById("but").style.border = "blue solid 2px";
 }
 
-function touchTheButtt() {
-  var x = document.getElementById("upgrade");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
+function buyUpgrade() {
+	dot = dot + 1;
+	document.getElementById("upgradeText").innerHTML = upgradeText[dot];
+	points = points - upgradeCost;
+	//upgradeCost = Math.round(upgradeCost * 1.5); // uncomment when time to publish
+	document.getElementById("upgradeCost").innerHTML = "Cost: " + upgradeCost + " points";
+	document.getElementById("butt").innerHTML = "You have " + points + " points";
+	progressBarProgress = progressBarProgress - upgradeCost;
+	document.getElementById("myBar").style.width = progressBarProgress + "%";
+	upgrades = upgrades + 1;
+	if (upgrades == 1){
+		pointsPerClick = pointsPerClick + 1;
+	}
+	if (upgrades == 2){
+	pointsPerClick = pointsPerClick + 1;
+	}
+	if (upgrades == 3){
+	level = level + 1;
+	document.getElementById("levelText").innerHTML = "level " + level;
+	}
+	if (upgrades == 3){
+	robot = "true"
+	document.getElementById("roboDiv").style.display = "block";
+	}
 
-function upgradeDiv() {
-  var x = document.getElementById("buttt");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
 }
-
 
 function upgradeFun() {
-alert("You cant get that yet! You need " + upgradeCost + " points!")
+	if (upgradeCost > points) {
+	null;
+	}
+	else if (upgradeCost <= points) {
+		buyUpgrade();
+	}
 }
 
 function mouseOverUpgradeBuyAble() {
@@ -84,8 +129,22 @@ function mouseOutUpgrade() {
 document.getElementById("upgrade").style.backgroundColor = "rgb(220,220,220)";
 }
 
-function canITouch() {
-touchTheButt()
-}
 
-// work on level up stuff
+
+function robotClick() {
+	if (robot == "true"){
+	myFunction();
+	}
+	else {
+	null;
+	}
+}
+// work on progressbar issues, satr at ten%
+//upgrade issues
+//w3 school js arrays
+//stop at lvl 99 
+//lvl 99 gets big fat prestige
+
+var mainGameLoop = window.setInterval(function() {
+  robotClick()
+}, 100)
