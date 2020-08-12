9d5e9fbac4344fee6094cfcdcddcda0ef62e7b49
Tragedy <54042397+Tragedy-dev@users.noreply.github.com>
2020-08-12 12:50:28 -0500

    Update insult.js

--- insult.js
+++ insult.js
@@ -1,12 +1,12 @@
-var insult1 = ["pig", "cow", "dirt", "gross", "poop", "butt", "sewer", "smelly"];
+var insult1 = ["pig", "cow", "dirt", "gross", "poop", "butt", "sewer", "smelly", "stinky"];
 var insult2 = ["eyed", "face", "toe", "brain", "head", "finger", "leg", "hair"];
 var insult3 = ["marble", "weed", "dumby", "idiot", "half-witt", "potato", "shorty", "weakling"]
 
 
 function insultgen() {
-    var num1 = Math.floor(Math.random() * 8);
-    var num2 = Math.floor(Math.random() * 8);
-    var num3 = Math.floor(Math.random() * 8);
+    var num1 = Math.floor(Math.random() * insult1.length);
+    var num2 = Math.floor(Math.random() * insult2.length);
+    var num3 = Math.floor(Math.random() * insult3.length);
 
     document.getElementById("insult").innerHTML = `You ${insult1[num1]} ${insult2[num2]} ${insult3[num3]}!`
 }
